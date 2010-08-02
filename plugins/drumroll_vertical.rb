8aa7fadec1c9798ac4be9fca71d4a9bccbbd8883
Sho Hashimoto <hashimoto@shokai.org>
2010-08-02 15:39:29 +0900

    drumroll vertical

--- plugins/drumroll_vertical.rb
+++ plugins/drumroll_vertical.rb
@@ -5,19 +5,19 @@ import 'javax.imageio.ImageIO'
 import 'java.awt.image.BufferedImage'
 
 
-module Drumroll_horizontal
+module Drumroll_vertical
 
-  def Drumroll_horizontal.glitch(img)
+  def Drumroll_vertical.glitch(img)
     img_result = BufferedImage.new(img.width, img.height, img.type)
     roll = 0
-    for y in 0...img.height do
-      roll = rand(img.width) if rand > 0.95
+    for x in 0...img.width do
+      roll = rand(img.height) if rand > 0.95
       roll = 0 if rand > 0.95
-      for x in 0...img.width do
+      for y in 0...img.height do
         pix = img.get_rgb(x, y)
-        x2 = x+roll
-        x2 -= img.width if x2 > img.width-1
-        img_result.set_rgb(x2, y, pix)
+        y2 = y+roll
+        y2 -= img.height if y2 > img.height-1
+        img_result.set_rgb(x, y2, pix)
       end
     end
     return img_result

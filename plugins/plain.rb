1f87934a58af2d9fb345e2de3d7108cea211b9b1
Sho Hashimoto <hashimoto@shokai.org>
2010-08-02 14:43:24 +0900

    plugin plain

---
+++ plugins/plain.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env jruby
+require 'java'
+import 'java.lang.System'
+import 'javax.imageio.ImageIO'
+import 'java.awt.image.BufferedImage'
+
+
+module Plain
+
+  def Plain.glitch(img)
+    img
+  end
+
+end

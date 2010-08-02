88bd9ffc45e52e10729c7c56c009e241411b5e2e
Sho Hashimoto <hashimoto@shokai.org>
2010-08-02 14:23:47 +0900

    add plugins

---
+++ plugins/colorshift_horizontal_broken.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env jruby
+require 'java'
+import 'java.lang.System'
+import 'javax.imageio.ImageIO'
+import 'java.awt.image.BufferedImage'
+
+
+module Colorshift_horizontal_broken
+
+  def Colorshift_horizontal_broken.glitch(img)
+    shifts = [0,0,0]
+    for y in 0...img.height do
+      if rand > 0.85
+        shifts = shifts.map{|j|
+          j = rand(255)-128
+        }
+      end
+      for x in 0...img.width do
+        pix = img.get_rgb(x, y)
+        r = pix >> 16 & 0xFF
+        g = pix >> 8 & 0xFF
+        b = pix & 0xFF
+        r += shifts[0]
+        g += shifts[1]
+        b += shifts[2]
+        pix = ((r << 16)&0xFF0000 | (g << 8)&0xFF00 | b)
+        img.set_rgb(x,y, pix)
+      end
+    end
+    return img
+  end
+
+end

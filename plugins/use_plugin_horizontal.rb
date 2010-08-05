ae7add43a3406a0fb4c169dd9cabd1835ff73d83
Sho Hashimoto <hashimoto@shokai.org>
2010-08-05 11:46:37 +0900

    rename

--- plugins/use_plugin_horizontal.rb
+++ plugins/use_plugin_horizontal.rb
@@ -5,9 +5,9 @@ import 'javax.imageio.ImageIO'
 import 'java.awt.image.BufferedImage'
 require File.dirname(__FILE__)+'/../glitch'
 
-module Useplugin_horizontal
+module Use_plugin_horizontal
 
-  def Useplugin_horizontal.glitch(img)
+  def Use_plugin_horizontal.glitch(img)
     img_result = img.get_subimage(0, 0, img.width, img.height)
     loop do
       range = Array.new
@@ -16,7 +16,6 @@ module Useplugin_horizontal
         break if rand > 0.98
       end
       img_part = img.get_subimage(0, range.first, img.width, range.size)
-      puts "#{img_part.width} #{img_part.height}"
       g = Glitch.new
       img_part, plugin = g.glitch(img_part)
       img_result.graphics.drawImage(img_part, 0, range.first, nil)

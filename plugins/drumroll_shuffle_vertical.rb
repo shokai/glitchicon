48b98eb4583ffcb2e187dadc60fa0582f8cc365a
Sho Hashimoto <hashimoto@shokai.org>
2010-11-23 13:59:35 +0900

    drumroll shuffle

--- plugins/drumroll_shuffle_vertical.rb
+++ plugins/drumroll_shuffle_vertical.rb
@@ -8,30 +8,17 @@ import 'java.awt.image.BufferedImage'
 module Drumroll_shuffle_vertical
 
   def Drumroll_shuffle_vertical.glitch(img)
-    # split rolls
-    rolls = Array.new
-    roll = Array.new
-    for x in 0...img.width do
-      roll << x
-      if rand > 0.95
-        rolls << roll
-        roll = Array.new
-      end
-    end
-    rolls << roll if roll.size > 0
+    regions = Array.new
+    for i in 0...img.height do regions << i end
+    size_ratio = img.height/100
+    regions = regions.split_nest((15*size_ratio).to_i,(40*size_ratio).to_i).sort_by{rand}
 
-    # shuffle
-    rand_rolls = Array.new
-    while rolls.size > 0 do
-      rand_rolls << rolls.delete_at(rand(rolls.size))
-    end
-
-    rand_rolls.flatten!
+    regions.flatten!
     img_result = BufferedImage.new(img.width, img.height, img.type)
     for x in 0...img.width do
       for y in 0...img.height do
         pix = img.get_rgb(x, y)
-        img_result.set_rgb(rand_rolls[x], y, pix)
+        img_result.set_rgb(regions[x], y, pix)
       end
     end
     return img_result

7a3db6444b2025889846e7bdce186e4f87fea8af
Sho Hashimoto <hashimoto@shokai.org>
2010-11-23 12:33:27 +0900

    add helper, Array::split_nest

---
+++ helper.rb
@@ -0,0 +1,21 @@
+
+class Array
+  def split_nest(min, max)
+    raise ArgunmentError 'split(min, max)  min should be less than max' if min > max
+    result = Array.new
+    _self = self.clone
+    p _self
+    loop do
+      tmp = Array.new
+      size = rand(max-min+1)+min
+      size.times do
+        break if _self.size < 1
+        tmp << _self.shift
+      end
+      result << tmp
+      break if _self.size < 1
+    end
+    return result
+  end
+end
+

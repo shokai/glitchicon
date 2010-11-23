968bedef34d916e192f1a2383bce649c1db87a4a
Sho Hashimoto <hashimoto@shokai.org>
2010-11-23 14:45:29 +0900

    filter plugin on test

--- glitch_test.rb
+++ glitch_test.rb
@@ -7,8 +7,9 @@ import 'java.awt.image.BufferedImage'
 
 
 if ARGV.size < 1
-  STDERR.puts 'require : input image'
+  STDERR.puts 'require : input image (plugin)'
   STDERR.puts 'jruby glitch_test.rb input.jpg'
+  STDERR.puts 'jruby glitch_test.rb input.jpg vertical'
   exit 1
 end
 
@@ -17,10 +18,12 @@ outdir = File.dirname(__FILE__)+"/test"
 Dir.mkdir(outdir) unless File.exists?(outdir)
 img_in = ImageIO.read(java.io.File.new(ARGV.shift))
 
+plugin_filter = ARGV.shift if ARGV.size > 0
 puts "#{img_in.width}x#{img_in.height}"
 
 g = Glitch.new
 g.plugins.each{|plugin|
+  next if plugin_filter != nil and !(plugin[1].to_s =~ /#{plugin_filter}/i)
   img = BufferedImage.new(img_in.width, img_in.height, img_in.type);  
   img.graphics.drawImage(img_in, 0, 0, nil)
   img, plugin = g.glitch(img, plugin)

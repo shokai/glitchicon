cb1ed4d750e37a09c113b0b7589c6fa4668f88bf
Sho Hashimoto <hashimoto@shokai.org>
2012-09-10 02:25:06 +0900

    add glitch_and_upload.sh

---
+++ glitch_and_upload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+TMP=/var/tmp/glitch_icon.png
+
+if test $# -lt 1
+then
+    echo "image file required."
+    echo "  e.g.  $0 /path/to/img.png"
+    exit 1
+fi
+
+cd `dirname $0`
+jruby create_icon.rb $1 $TMP
+jruby upload.rb $TMP
